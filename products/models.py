7325510740e93f169f5bd2fbc09822ba3deeb8ad
fredboys <freddieboys@hotmail.co.uk>
2023-04-04 14:00:34 +0000

    image validator

---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    """
+    Product model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    link = models.TextField(blank=True)
+    price = models.IntegerField()
+    location = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_lzlyyd', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.name}'
