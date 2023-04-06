89e3455264ec36bdfb8f1cae86deda7426655cd4
fredboys <freddieboys@hotmail.co.uk>
2023-04-06 19:38:05 +0000

    create product count

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -9,8 +10,16 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        products_count=Count('owner__product', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'products_count'
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
